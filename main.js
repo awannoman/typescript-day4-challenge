83a9760397dcdab2c3434bc48c6d25b232107668
awannoman <160035058+awannoman@users.noreply.github.com>
2024-03-06 23:27:12 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+"use strict";
+//Q10
+// Noman, 05/03/2024
+// This program prints a personal message.
+let myName = "Noman";
+console.log('hello', myName, "you can do it?");
+// Q11
+let friendName = ["daniyal", "ali", "faraz", "tayyab"];
+console.log(friendName[0]);
+console.log(friendName[1]);
+console.log(friendName[2]);
+console.log(friendName[3]);
+// Q12
+let friendName = ["daniyal", "ali", "faraz", "tayyab"];
+let message = "you can do it nothing is impossible";
+console.log("hello", friendName[0], message);
+console.log("hello", friendName[1], message);
+console.log("hello", friendName[2], message);
+console.log("hello", friendName[3], message);
